a5f157c613ebc85cf2ef7c0ee3a01115b60d9217
Turchanov-Denis <turchanovdenis786@gmail.com>
2023-12-15 14:38:47 +0200

    hotfix

--- algorithmsAndStructures[part1]/lab13_17/lab17.py
+++ algorithmsAndStructures[part1]/lab13_17/lab17.py
@@ -1,20 +1,18 @@
-class TreeNode:
+class Node:
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
 
-
 def insert(root, key):
     if root is None:
-        return TreeNode(key)
+        return Node(key)
     else:
-        if root.key < key:
-            root.right = insert(root.right, key)
-        else:
+        if key < root.key:
             root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
     return root
-
 
 def delete(root, key):
     if root is None:
@@ -30,66 +28,66 @@ def delete(root, key):
         elif root.right is None:
             return root.left
 
-        root.key = minValueNode(root.right)
+        root.key = minValueNode(root.right).key
         root.right = delete(root.right, root.key)
 
     return root
-
 
 def minValueNode(node):
     current = node
     while current.left is not None:
         current = current.left
-    return current.key
-
+    return current
 
 def search(root, key):
     if root is None or root.key == key:
         return root
 
-    if root.key < key:
-        return search(root.right, key)
-    return search(root.left, key)
+    if key < root.key:
+        return search(root.left, key)
+    return search(root.right, key)
 
+def print_tree(node, level=0, prefix="Root: "):
+    if node is not None:
+        print(" " * (level * 4) + prefix + str(node.key))
+        if node.left or node.right:
+            print_tree(node.left, level + 1, "L --- ")
+            print_tree(node.right, level + 1, "R --- ")
 
-def inorder_traversal(root):
-    if root:
-        inorder_traversal(root.left)
-        print(root.key, end=' ')
-        inorder_traversal(root.right)
-
-
-def menu():
-    return input("Меню:\n1. Добавить вершину\n2. Удалить вершину\n3. Поиск вершины\n4. Вывести БДП\n5. Выход\nВыберите операцию: ")
-
+def print_menu():
+    print("\nМеню:")
+    print("1. Добавить вершину")
+    print("2. Удалить вершину")
+    print("3. Найти вершину")
+    print("4. Вывести БДП")
+    print("5. Выйти")
 
 def main():
     root = None
 
     while True:
-        choice = menu()
+        print_menu()
+        choice = int(input("Выберите операцию (1-5): "))
 
-        if choice == '1':
-            key = int(input("Введите значение вершины: "))
+        if choice == 1:
+            key = int(input("Введите ключ для добавления: "))
             root = insert(root, key)
-        elif choice == '2':
-            key = int(input("Введите значение вершины для удаления: "))
+        elif choice == 2:
+            key = int(input("Введите ключ для удаления: "))
             root = delete(root, key)
-        elif choice == '3':
-            key = int(input("Введите значение вершины для поиска: "))
-            result = search(root, key)
-            if result:
-                print(f"Вершина с ключом {key} найдена.")
+        elif choice == 3:
+            key = int(input("Введите ключ для поиска: "))
+            if search(root, key):
+                print(f"Вершина с ключом {key} найдена в БДП.")
             else:
-                print(f"Вершина с ключом {key} не найдена.")
-        elif choice == '4':
-            print("БДП:")
-            inorder_traversal(root)
-            print()
-        elif choice == '5':
+                print(f"Вершина с ключом {key} не найдена в БДП.")
+        elif choice == 4:
+            print("БДП (графический вывод):")
+            print_tree(root)
+        elif choice == 5:
             break
         else:
-            print("Некорректный ввод. Попробуйте еще раз.")
+            print("Неверный выбор. Попробуйте снова.")
 
 if __name__ == "__main__":
     main()
