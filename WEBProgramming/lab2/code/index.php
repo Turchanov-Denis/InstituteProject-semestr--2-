86544c3752d665898536df1658b4562a7b4cfbb5
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2024-03-07 01:28:58 +0200

    web lab2[->task15]

--- WEBProgramming/lab2/code/index.php
+++ WEBProgramming/lab2/code/index.php
@@ -35,7 +35,7 @@ $daysPerLanguage = $days / $numLanguages;
 echo "average days : $daysPerLanguage days\n";
 echo "\n \n";
 // task12
-echo 8**2;
+echo 8 ** 2;
 echo "\n \n";
 // task13
 $myNum = 233;
@@ -45,5 +45,96 @@ $answer *= 2;
 $answer -= 2;
 $answer /= 2;
 $answer -= $myNum;
-echo"$answer \n";
+echo "$answer \n";
 // task14
+$a = 10;
+$b = 3;
+echo $a % $b;
+echo "\n";
+//
+if (0 == $a % $b) {
+    echo "divide ";
+    echo $a / $b;
+    echo "\n";
+} else {
+    echo "not divide, remainder of the division: ";
+    echo $a % $b;
+    echo "\n";
+}
+//
+$st = pow(2, 10);
+$s = sqrt(245);
+$randomList = [4, 2, 5, 19, 13, 0, 10];
+$ww = 0;
+foreach ($randomList as $value) {
+    $ww += pow($value, 2);
+}
+$ww = sqrt($ww);
+//
+$n0 = round(sqrt(349), 0);
+$n1 = round(sqrt(349), 1);
+$n2 = round(sqrt(349), 2);
+
+$numC = ceil(sqrt(587));
+$numF = floor(sqrt(587));
+$arr = [
+    "ceil" => $numC,
+    "floor" => $numF,
+];
+//
+$arr = [4, -2, 5, 19, -130, 0, 10];
+$min = min($arr);
+$max = max($arr);
+//
+echo rand(1, 100);
+echo "\n";
+$ArrayOfRandNum = [];
+for ($i = 0; $i < 10; $i++) {
+    $arrayOfRandom[$i] = rand(1, 100);
+}
+echo "\n";
+//
+$a = 111;
+$b = 222;
+$res = abs($a - $b);
+echo $res; echo "\n";
+// alternative
+$a = -111;
+$b = 2;
+$res = abs($a - $b);
+echo $res;
+
+$arrayOfNum = [1, 2, -1, -2, 3, -3];
+$newArr=array();
+foreach ($arrayOfNum as $value) {
+    $value = abs($value);
+    array_push($newArr, $value);
+}
+echo "abs ";
+print_r ($newArr);
+echo "\n";
+//
+$ControlNum = rand(1, 1000);
+$i = 1;
+$arr = array();
+while ($i <= $ControlNum) {
+    if (0 === $ControlNum % $i) {
+        array_push($arr, $i);
+    }
+    $i++;
+}
+echo " number of divide: $ControlNum";
+print_r ($arr);
+
+$arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+$ans = 0;
+$counter = 0;
+foreach ($arr1 as $value) {
+    if ($ans > 10) {
+        break;
+    }
+    $ans += $value;
+    $counter++;
+}
+echo "counter: $counter";
+// task15
