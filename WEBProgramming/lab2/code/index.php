684741841b858adc223ad86229401bc3d493fb6a
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2024-03-07 00:33:59 +0200

    web lab2[task1]

--- WEBProgramming/lab2/code/index.php
+++ WEBProgramming/lab2/code/index.php
@@ -1,2 +1,13 @@
 <?php
-echo phpinfo();
+// task1
+/* Imagine a lot of code here */
+$very_bad_unclear_name = "15 chicken wings";
+
+
+// Write your code here:
+$order = &$very_bad_unclear_name;
+$order = $order . "cat";
+
+// Don't change the line below
+echo "\nYour order is: $order.";
+// task1
