aa4a41f8096e26de21ad6816674fad6640cfdc67
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2024-03-19 03:30:23 +0200

    alg[llab6][devide to edge+base alg]

--- algorithmsAndStructures[part2]/lab6/lab6.py
+++ algorithmsAndStructures[part2]/lab6/lab6.py
@@ -1,12 +1,13 @@
 import json
 
+
 class Graph():
     def __init__(self):
-        self.graph = {} # {'A': {'B': 5, 'D': 3}, 'B': {'A': 5, 'C': 2}} // example
+        self.graph = {}  # {'A': {'B': 5, 'D': 3}, 'B': {'A': 5, 'C': 2}} // example
 
     @classmethod
     def from_json(cls, file_path):
-        with open(file_path, 'r') as file:  
+        with open(file_path, 'r') as file:
             data = json.load(file)
         graph = cls()
         for i, u in enumerate(data["nodes"]):
@@ -24,7 +25,34 @@ class Graph():
             self.graph[v] = {}
         self.graph[v][u] = w
 
+    def find(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.find(parent, parent[i])
+
+    def kruskala(self):
+        result = []
+        edges = []
+        parent = {}
+        for u in self.graph.keys():
+            for v, w in self.graph[u].items():
+                edges.append((u, v, w))
+        edges.sort(key=lambda x: x[2])
+
+        for u, v, _ in edges:
+            parent[u] = u
+            parent[v] = v
+        print(parent)
+        for edge in edges:
+            u, v, w = edge
+            x = self.find(parent, u)
+            y = self.find(parent, v)
+            if x != y:
+                result.append((u, v, w))
+
+        return result
+
 
 if __name__ == "__main__":
     graph = Graph.from_json("adjacency_matrix_lab_6.json")
-    print(graph.graph)
+    print(graph.kruskala())
