4f5b12ae3ea8ef134ab37b9d22376ae2f129ccd1
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2024-03-30 17:27:14 +0200

    web lab3[task3->form+save.php]

--- WEBProgramming/lab3/code/index.php
+++ WEBProgramming/lab3/code/index.php
@@ -8,19 +8,23 @@
 </head>
 
 <body>
-    <form action="save.php" method="post">
-        <label for="email">Email</label>
-        <input type="email" name="email" required>
-        <label for="category">Category</label>
-        <select name="category" required>
-            <option value="cars">Cars</option>
-            <option value="other">Other</option>
-        </select>
-        <label for="title">Title</label>
-        <input type="text" name="title" required>
-        <label for="description">Description</label>
-        <textarea rows="3" name="description"></textarea>
-    </form>
+    <div id="form">
+        <form action="save.php" method="post">
+            <label for="email">Email</label>
+            <input type="email" name="email" required>
+            <label for="category">Category</label>
+            <select name="category" required>
+                <option value="cars">Cars</option>
+                <option value="other">Other</option>
+            </select>
+            <label for="title">Title</label>
+            <input type="text" name="title" required>
+            <label for="description">Description</label>
+            <textarea rows="3" name="description"></textarea>
+            <input type="submit" value="save">
+        </form>
+    </div>
+    
 </body>
 
 </html>
