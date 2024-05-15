9a38177efc7b977459efab847ace77539a2c60a7
Turchanov-Denis <96124363+Turchanov-Denis@users.noreply.github.com>
2024-05-15 23:11:13 +0200

    dm [lab2 update]

--- DiscreteMath/lab2/task1.py
+++ DiscreteMath/lab2/task1.py
@@ -3,10 +3,11 @@ from itertools import combinations
 word = "комбинаторика"
 word_length = 5
 
-# Генерируем все комбинации из букв слова длиной word_length
 combs = combinations(word, word_length)
+for item in combs:
+    print(item)
 
-# Считаем количество комбинаций
+combs = combinations(word, word_length)
 num_combinations = sum(1 for _ in combs)
-
 print("Количество 5-буквенных слов из слова 'комбинаторика':", num_combinations)
+
